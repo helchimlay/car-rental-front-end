03e4c7a7c85255c6dc952fba43b265d352cbeefc
MarcinSt04 <2004marcin.stepien@gmail.com>
2022-07-05 10:20:50 +0200

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,24 +1,24 @@
-import React, { useState, useEffect } from "react"
-import "./Footer.css"
+import React, { useState, useEffect } from 'react';
+import './Footer.css';
 
 const Footer = () => {
-  const [thisDate, setThisDate] = useState(null)
+  const [thisDate, setThisDate] = useState(null);
   useEffect(() => {
-    setThisDate(new Date().getFullYear())
-  }, [])
+    setThisDate(new Date().getFullYear());
+  }, []);
 
   return (
     <footer>
-      <div className="footer-info">
+      <div className='footer-info'>
         <h2>CarRental</h2>
         <h3>Najlepszy serwis z samochodami w Polsce!</h3>
         <p>Dziękujemy że nas odwiedziłeś, mamy nadzieję że niedługo wrócisz.</p>
       </div>
-      <div className="footer-copyright">
+      <div className='footer-copyright'>
         <p>CarRental {thisDate} wszelkie prawa zastrzeżone</p>
       </div>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
