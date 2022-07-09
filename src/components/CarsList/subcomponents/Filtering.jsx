166271980b8e2375183a476ee6b729ab5aa529e2
MarcinSt04 <2004marcin.stepien@gmail.com>
2022-07-09 13:17:37 +0200

    updated input styles, updated URL when user use filtering

--- src/components/CarsList/subcomponents/Filtering.jsx
+++ src/components/CarsList/subcomponents/Filtering.jsx
@@ -36,7 +36,10 @@ const Filtering = props => {
     e.preventDefault();
     if (selectedBrand || selectedFuel || selectedCategory) {
       navigate(
-        `/lista-samochodow?marka=${selectedBrand}&rodzaj_paliwa=${selectedFuel}&kategoria_cenowa=${selectedCategory}`
+        '/lista-samochodow?' +
+          (selectedBrand ? `marka=${selectedBrand}` : '') +
+          (selectedFuel ? `&rodzaj_paliwa=${selectedFuel}` : '') +
+          (selectedCategory ? `&kategoria_cenowa=${selectedCategory}` : '')
       );
       getCarsListByFiltering(
         selectedBrand,
@@ -98,17 +101,19 @@ const Filtering = props => {
           ) : null}
         </div>
         <button type='submit'>
-          <svg
-            xmlns='http://www.w3.org/2000/svg'
-            width='20'
-            height='20'
-            fill='currentColor'
-            className='bi bi-search'
-            viewBox='0 0 16 16'
-          >
-            <path d='M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z' />
-          </svg>
-          Szukaj
+          <span>
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              width='16'
+              height='16'
+              fill='currentColor'
+              className='bi bi-search'
+              viewBox='0 0 16 16'
+            >
+              <path d='M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z' />
+            </svg>
+            Szukaj
+          </span>
         </button>
       </form>
     </section>
