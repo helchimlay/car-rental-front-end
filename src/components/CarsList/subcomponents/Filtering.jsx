6032a348d0718fb3f4aa636016f99a378ce67995
MarcinSt04 <2004marcin.stepien@gmail.com>
2022-07-09 23:44:08 +0200

    optimized filtering

--- src/components/CarsList/subcomponents/Filtering.jsx
+++ src/components/CarsList/subcomponents/Filtering.jsx
@@ -69,53 +69,49 @@ const Filtering = props => {
 
   return (
     <section className='filter-fields'>
-      <form onSubmit={handleSubmit}>
-        <div className='select-brand'>
-          {selectOptions ? (
+      {selectOptions && (
+        <form onSubmit={handleSubmit}>
+          <div className='select-brand'>
             <FilterList
               change={handleBrandChange}
               items={selectOptions.brands}
               label={'Marka'}
               value={selectedBrand}
             />
-          ) : null}
-        </div>
-        <div className='select-fuel'>
-          {selectOptions ? (
+          </div>
+          <div className='select-fuel'>
             <FilterList
               change={handleFuelChange}
               items={selectOptions.types_of_fuels}
               label={'Rodzaj paliwa'}
               value={selectedFuel}
             />
-          ) : null}
-        </div>
-        <div className='select-category'>
-          {selectOptions ? (
+          </div>
+          <div className='select-category'>
             <FilterList
               change={handleCategoryChange}
               items={selectOptions.categories_of_price}
               label={'Kategoria cenowa'}
               value={selectedCategory}
             />
-          ) : null}
-        </div>
-        <button type='submit'>
-          <span>
-            <svg
-              xmlns='http://www.w3.org/2000/svg'
-              width='16'
-              height='16'
-              fill='currentColor'
-              className='bi bi-search'
-              viewBox='0 0 16 16'
-            >
-              <path d='M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z' />
-            </svg>
-            Szukaj
-          </span>
-        </button>
-      </form>
+          </div>
+          <button type='submit'>
+            <span>
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                width='16'
+                height='16'
+                fill='currentColor'
+                className='bi bi-search'
+                viewBox='0 0 16 16'
+              >
+                <path d='M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z' />
+              </svg>
+              Szukaj
+            </span>
+          </button>
+        </form>
+      )}
     </section>
   );
 };
