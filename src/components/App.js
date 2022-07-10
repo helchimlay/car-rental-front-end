e8ceb197be1a13a9fc0130d1e3a76c9edc5fab73
Jakub Olechowski <kubaole2@op.pl>
2022-07-10 22:44:53 +0200

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,13 @@
-import React, { Component } from 'react';
-import { Routes, Route } from 'react-router-dom';
-import './App.css';
+import React, { Component } from "react"
+import { Routes, Route } from "react-router-dom"
+import "./App.css"
 
-import Header from './Header/Header';
-import Footer from './Footer/Footer';
-import HomePage from './HomePage/HomePage';
-import Car from './Car/Car';
-import CarsList from './CarsList/CarsList';
+import Header from "./Header/Header"
+import Footer from "./Footer/Footer"
+import HomePage from "./HomePage/HomePage"
+import Car from "./Car/Car"
+import CarsList from "./CarsList/CarsList"
+import Calculator from "./Calculator/Calculator"
 
 const App = () => {
   return (
@@ -14,14 +15,15 @@ const App = () => {
       <Header />
 
       <Routes>
-        <Route exact path='/' element={<HomePage></HomePage>} />
-        <Route path='/lista-samochodow' element={<CarsList></CarsList>} />
-        <Route path='/lista-samochodow/:carSlug' element={<Car></Car>} />
+        <Route exact path="/" element={<HomePage></HomePage>} />
+        <Route path="/lista-samochodow" element={<CarsList></CarsList>} />
+        <Route path="/lista-samochodow/:carSlug" element={<Car></Car>} />
+        <Route path="/kalkulator" element={<Calculator></Calculator>} />
       </Routes>
 
       <Footer />
     </>
-  );
-};
+  )
+}
 
-export default App;
+export default App
---
+++ src/components/Calculator/Calculator.jsx
@@ -0,0 +1,84 @@
+import React from "react"
+import "./Calculator.css"
+
+const Calculator = () => {
+  return (
+    <>
+      <main className="calculator">
+        <section className="calculator-form">
+          <div className="calculator-car-img">
+            <img
+              className="calculator-car-img"
+              src="https://via.placeholder.com/375x250"
+              alt="car"
+            />
+          </div>
+          <div className="calculator-car-info">
+            <h1 className="calculator-car-model">Seat cordoba 123</h1>
+            <ul>
+              <li>2000</li>
+              <li className="calculator-li">60 000 km</li>
+              <li className="calculator-li">diesel</li>
+            </ul>
+          </div>
+          <h1 className="calculator-title">
+            Oblicz koszt wyposażenia samochodu
+          </h1>
+          <div className="div-form">
+            <form>
+              <div className="form-div-left">
+                <label htmlFor="">Termin wyposażenia samochodu</label>
+                <br />
+                Od:&nbsp;&nbsp;
+                <input type="date" name="rent-since" id="rent-since" />
+                &nbsp; do:&nbsp;&nbsp;
+                <input type="date" name="rent-to" id="rent-to" />
+                <br />
+                <label htmlFor="adress">Lokalizacja odbioru samochodu</label>
+                <br />
+                <input
+                  placeholder="Wpisz adres..."
+                  type="text"
+                  name="adress"
+                  id="adress"
+                />
+                <br />
+              </div>
+              <div className="form-div-right">
+                <label htmlFor="">Rok otrzymania prawa jazdy</label>
+                <br />
+                <input
+                  type="date"
+                  name="driving-license-received"
+                  id="driving-license-received"
+                />
+                <br />
+                <label htmlFor="kilometers-to-drive">
+                  Szacunkowa ilość kilometrów do przejechania
+                </label>
+                <br />
+                <input
+                  className="range-display"
+                  type="range"
+                  name="kilometers-to-drive"
+                  id="kilometers-to-drive"
+                  min="0"
+                  max="1000"
+                />
+              </div>
+              <div className="calculator-submit">
+                <input type="submit" value="Oblicz koszt" />
+              </div>
+            </form>
+          </div>
+          <div className="div-price">
+            <h2 className="price-title">Koszt wypożyczenia:</h2>
+            <div className="price-display">1234 zł</div>
+          </div>
+        </section>
+      </main>
+    </>
+  )
+}
+
+export default Calculator
