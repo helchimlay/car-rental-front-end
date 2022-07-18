26f572c2f7d2b554c684085910a596da51841afc
Jakub Olechowski <kubaole2@op.pl>
2022-07-18 19:50:16 +0200

    char

--- src/components/Calculator/subcomponents/Summary.jsx
+++ src/components/Calculator/subcomponents/Summary.jsx
@@ -1,72 +1,76 @@
 import React from "react"
 import "./Summary.css"
 
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js"
 import { Pie } from "react-chartjs-2"
 
+ChartJS.register(ArcElement, Tooltip, Legend)
+
 const Summary = (props) => {
-  const { priceBrutto, priceNetto, numberOfRentDays, deliveryFee, fuelPrice } =
-    props.summary
+  const {
+    priceBrutto,
+    priceNetto,
+    numberOfRentDays,
+    deliveryFee,
+    fuelPrice,
+    priceOfFuelUsedWhileRented,
+  } = props.summary
   const { kilometersToDrive, future_location } = props.rentInfo
   const priceForOneNight = props.priceForOneNight
   const presentLocation = props.presentLocation
+
+  const vatPrice = priceNetto * 0.23
+
+  const rentPrice = priceForOneNight * numberOfRentDays
 
   return (
     <section className="calculator-summary">
       {/* Koszty paliwa   |   Podatek VAT   |    Cena wypożyczenia samochodu   |   Koszt dostarczenia samochodu*/}
       <div className="calculator-summary-graph">
-        {/* <Pie
+        <Pie
           data={{
-            labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+            labels: [
+              "Koszty paliwa",
+              "Podatek VAT",
+              "Cena wypożyczenia samochodu",
+              "Koszt dostarczenia samochodu",
+            ],
             datasets: [
               {
                 label: "# of votes",
-                data: [12, 19, 3, 5, 2, 3],
+                data: [
+                  priceOfFuelUsedWhileRented,
+                  vatPrice,
+                  rentPrice,
+                  deliveryFee,
+                ],
                 backgroundColor: [
                   "rgba(255, 99, 132, 0.2)",
                   "rgba(54, 162, 235, 0.2)",
                   "rgba(255, 206, 86, 0.2)",
                   "rgba(75, 192, 192, 0.2)",
-                  "rgba(153, 102, 255, 0.2)",
-                  "rgba(255, 159, 64, 0.2)",
                 ],
                 borderColor: [
                   "rgba(255, 99, 132, 1)",
                   "rgba(54, 162, 235, 1)",
                   "rgba(255, 206, 86, 1)",
                   "rgba(75, 192, 192, 1)",
-                  "rgba(153, 102, 255, 1)",
-                  "rgba(255, 159, 64, 1)",
                 ],
                 borderWidth: 1,
               },
-              // {
-              //   label: 'Quantity',
-              //   data: [47, 52, 67, 58, 9, 50],
-              //   backgroundColor: 'orange',
-              //   borderColor: 'red',
-              // },
             ],
           }}
           height={400}
           width={600}
           options={{
             maintainAspectRatio: false,
-            scales: {
-              yAxes: [
-                {
-                  ticks: {
-                    beginAtZero: true,
-                  },
-                },
-              ],
-            },
             legend: {
               labels: {
                 fontSize: 25,
               },
             },
           }}
-        /> */}
+        />
       </div>
       <div className="calculator-summary-info">
         <p className="price-brutto">
