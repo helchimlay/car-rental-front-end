aa736e0ea65a487e238eb94aab22313ac458dcb0
Jakub Olechowski <kubaole2@op.pl>
2022-07-18 14:16:38 +0200

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import './App.css';
+import React from "react"
+import { Routes, Route } from "react-router-dom"
+import "./App.css"
 
-import { routes } from '../config/routes/routes';
-import Nav from './Nav/Nav';
-import Footer from './Footer/Footer';
-import HomePage from './HomePage/HomePage';
-import ErrorPage from './ErrorPage/ErrorPage';
-import Car from './Car/Car';
-import CarsList from './CarsList/CarsList';
-import Calculator from './Calculator/Calculator';
+import { routes } from "../config/routes/routes"
+import Nav from "./Nav/Nav"
+import Footer from "./Footer/Footer"
+import HomePage from "./HomePage/HomePage"
+import ErrorPage from "./ErrorPage/ErrorPage"
+import Car from "./Car/Car"
+import CarsList from "./CarsList/CarsList"
+import Calculator from "./Calculator/Calculator"
 
 const App = () => {
   return (
@@ -17,7 +17,7 @@ const App = () => {
       <Nav />
 
       <Routes>
-        <Route exact path='/' element={<HomePage></HomePage>} />
+        <Route exact path="/" element={<HomePage></HomePage>} />
         <Route path={routes.cars} element={<CarsList></CarsList>} />
         <Route path={routes.car()} element={<Car></Car>} />
         <Route path={routes.calculator} element={<Calculator></Calculator>} />
@@ -25,12 +25,12 @@ const App = () => {
           path={routes.carCalculator()}
           element={<Calculator></Calculator>}
         />
-        <Route path='*' element={<ErrorPage></ErrorPage>} />
+        <Route path="*" element={<ErrorPage></ErrorPage>} />
       </Routes>
 
       <Footer />
     </>
-  );
-};
+  )
+}
 
-export default App;
+export default App
